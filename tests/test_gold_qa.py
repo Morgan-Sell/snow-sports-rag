e86191c3e9f8464a1907ea2d294d747822f58de0
Morgan-Sell <morganpsell@gmail.com>
2026-04-24 18:54:10 -0700

    fixed formatting error - take 2

--- tests/test_gold_qa.py
+++ tests/test_gold_qa.py
@@ -7,9 +7,9 @@ from pathlib import Path
 
 import pytest
 from snow_sports_rag.evaluation import (
-    GoldItem,
     default_gold_qa_path,
-    load_gold_qa
+    load_gold_qa,
+    GoldItem,
 )
 
 
